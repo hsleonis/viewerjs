83f9851ae53f224caf1a6edc81a53d1ebf9ad4d0
MD. Hasan Shahriar <hsleonis@gmail.com>
2016-08-31 11:55:45 +0600

    Mobile theme

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <div>
             <!--<iframe src="http://localhost/test/doc.php" style="width:100%;height:550px;border:0"></iframe>-->
             <!--<iframe id="doc" style="margin:0 auto; width:100%; float:none;height:500px;border:0;" src="https://docs.google.com/viewerng/viewer?url=http://kmmc.in/wp-content/uploads/2014/01/lesson2.pdf&hl=bn&embedded=true"></iframe>-->
+            <!--<iframe id="doc" style="margin:0 auto; width:100%; float:none;height:500px;border:0;" src="https://docs.google.com/viewerng/viewer?url=http://kmmc.in/wp-content/uploads/2014/01/lesson2.pdf&hl=bn&mobile=true"></iframe>-->
             <iframe src = "resources/ViewerJS/#../files/doc.pdf" width='566' height='800' allowfullscreen webkitallowfullscreen></iframe>
             <iframe src = "resources/ViewerJS/#../files/test.odt" width='566' height='800' allowfullscreen webkitallowfullscreen></iframe>
         </div>
